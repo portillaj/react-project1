4b6ad9dc616bf0678b50d293c03b7d9bc94a37a2
portillaj <portillaj85@yahoo.com>
2017-10-20 23:35:17 -0400

    simple react app practicing with state and props

---
+++ myapp/src/components/header.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            keywords: ''
+        }
+    }
+
+    inputChange(event) {
+        this.setState({keywords: event.target.value})
+    }
+
+    render() {
+        return ( 
+            <header>
+                <div className="logo" onClick={()=> {console.log('clicked')}}>Logo</div>
+                <input onChange={this.inputChange.bind(this)}/>
+            </header>
+        )
+    } 
+}
+
+export default Header;
---
+++ myapp/src/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+//Components
+import Header from './components/header';
+import JSON from './db.json';
+import NewsList from './components/news_list';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            news: JSON
+        }
+    }
+    render() {
+        return (
+            <div>
+                <Header />
+                <NewsList news={this.state.news}/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
